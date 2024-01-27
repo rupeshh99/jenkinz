a2605ef84ef9d8afb401b099b35a304da4efd471
rupeshh99 <rupeshhh94@gmail.com>
2024-01-27 19:14:18 +0530

    Initial commit

---
+++ src/main/java/com/spring/jenkinz/JenkinzApplication.java
@@ -0,0 +1,24 @@
+package com.spring.jenkinz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class JenkinzApplication {
+
+	static Logger logger= LoggerFactory.getLogger(JenkinzApplication.class);
+
+	@PostConstruct
+	public void start(){
+		logger.info("application has been started");
+	}
+	public static void main(String[] args) {
+		logger.info("application has been executed");
+		SpringApplication.run(JenkinzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jenkinz/JenkinzApplicationTests.java
@@ -0,0 +1,19 @@
+package com.spring.jenkinz;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinzApplicationTests {
+
+	static Logger logger= LoggerFactory.getLogger(JenkinzApplication.class);
+
+
+	@Test
+	void contextLoads() {
+		logger.info("test case executing");
+	}
+
+}
